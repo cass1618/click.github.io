6e194909c77200bd0ad8dd440460bc243a48cf96
cass1618 <cass16180@gmail.com>
2021-03-22 22:06:30 -0700

    Add files and photos

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+
+jQuery("#cat").click( function image ()
+{
+  alert("this is a cat");
+})
+
+jQuery("#dog").click( function image ()
+{
+  alert("this is a dog");
+})
+
+jQuery("#platypus").click( function image ()
+{
+  alert("this is a platypus");
+})
+
+jQuery("#sun").click( function image ()
+{
+  alert("this is the sun");
+})
+
+jQuery("#beer").click( function image ()
+{
+  alert("this is a beer");
+})
+
